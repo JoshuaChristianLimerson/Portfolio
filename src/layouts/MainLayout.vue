96f7ee3ebd506c1a7a04feff5bfb53e188b210a4
joshua.limerson <joshua.limerson@cp.co.id>
2024-08-05 11:28:50 +0700

    fix responsive

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,8 +7,41 @@
       :style="{ height: drawer ? '100vh' : '90px', zIndex: 99 }"
     >
       <q-header class="text-white" style="background: transparent">
-        <q-toolbar>
-          <q-toolbar-title class="row justify-center"> Title </q-toolbar-title>
+        <q-toolbar class="row justify-center">
+          <q-tabs
+            v-model="tab"
+            v-if="$q.screen.gt.xs"
+            active-color="primary"
+            indicator-color="primary"
+          >
+            <q-tab
+              name="tab1"
+              label="Home"
+              class="text-black"
+              @click="moveTo('home')"
+            />
+            <q-tab
+              name="tab2"
+              label="Features"
+              class="text-black"
+              @click="moveTo('features')"
+            />
+            <q-tab
+              name="tab3"
+              label="Contact Us"
+              class="text-black"
+              @click="moveTo('contact-us')"
+            />
+          </q-tabs>
+          <q-btn
+            flat
+            @click="drawer = !drawer"
+            round
+            dense
+            icon="menu"
+            color="black"
+            v-if="$q.screen.xs"
+          />
         </q-toolbar>
       </q-header>
       <q-drawer
@@ -70,13 +103,22 @@
 
   <div class="gradient-background row justify-center items-center">
     <!-- <router-view /> -->
-    <div class="q-px-xs-md column">
-      <q-card bordered class="my-card q-px-xl">
+    <div class="column" style="padding-left: 7px; padding-right: 7px">
+      <q-card bordered class="my-card">
         <div class="card-content">
           <q-card-section>
-            <h1 class="text-white text-bold q-pb-none">
+            <h1
+              class="text-white text-weight-bold text-center"
+              v-if="$q.screen.gt.xs"
+            >
               Joshua Christian Limerson
             </h1>
+            <h2
+              class="text-white text-weight-bold text-center"
+              v-if="$q.screen.xs"
+            >
+              Joshua Christian Limerson
+            </h2>
             <div class="row justify-center">
               <p>
                 I am
@@ -88,7 +130,7 @@
           <q-separator inset />
 
           <q-card-section class="row justify-center">
-            <div class="card">
+            <div class="socialList">
               <a class="socialContainer containerOne" href="#">
                 <svg viewBox="0 0 16 16" class="socialSvg instagramSvg">
                   <path
@@ -109,14 +151,6 @@
                 <svg viewBox="0 0 448 512" class="socialSvg linkdinSvg">
                   <path
                     d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                  ></path>
-                </svg>
-              </a>
-
-              <a class="socialContainer containerFour" href="#">
-                <svg viewBox="0 0 16 16" class="socialSvg whatsappSvg">
-                  <path
-                    d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"
                   ></path>
                 </svg>
               </a>
@@ -152,6 +186,7 @@ onMounted(() => {
     }
   }
 });
+const tab = ref('tab1');
 
 const drawer = ref(false);
 const moveTo = (id: string) => {
@@ -201,19 +236,17 @@ const moveTo = (id: string) => {
 }
 
 .my-card:hover {
-  box-shadow: 0 6px 40px rgba(0, 0, 0, 0.2); /* Enhanced shadow for zoom effect */
+  box-shadow: 0 6px 40px rgba(0, 0, 0, 0.3); /* Enhanced shadow for zoom effect */
 }
 
-.card {
+.socialList {
   width: fit-content;
   height: fit-content;
-  background-color: rgb(238, 238, 238);
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 25px 25px;
   gap: 20px;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.055);
   border-radius: 5em;
 }
 
