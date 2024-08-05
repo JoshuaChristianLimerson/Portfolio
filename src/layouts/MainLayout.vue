cb49213fc52091e0524d87afdcae8923b1dfb4f7
Joshua Christian Limerson <jcljoshualim@gmail.com>
2024-08-05 22:00:19 +0700

    fix links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,25 +11,14 @@
           <q-tabs
             v-model="tab"
             v-if="$q.screen.gt.xs"
-            active-color="primary"
-            indicator-color="primary"
+            active-color="dark"
+            indicator-color="dark"
           >
-            <q-tab
-              name="tab1"
-              label="Home"
-              class="text-black"
-              @click="moveTo('home')"
-            />
-            <q-tab
-              name="tab2"
-              label="Features"
-              class="text-black"
-              @click="moveTo('features')"
-            />
+            <q-tab name="tab1" label="Home" @click="moveTo('home')" />
+            <q-tab name="tab2" label="Features" @click="moveTo('features')" />
             <q-tab
               name="tab3"
               label="Contact Us"
-              class="text-black"
               @click="moveTo('contact-us')"
             />
           </q-tabs>
@@ -104,25 +93,28 @@
   <div class="gradient-background row justify-center items-center">
     <!-- <router-view /> -->
     <div class="column" style="padding-left: 7px; padding-right: 7px">
-      <q-card bordered class="my-card">
+      <q-card bordered class="my-card q-px-lg">
         <div class="card-content">
           <q-card-section>
-            <h1
+            <h2
               class="text-white text-weight-bold text-center"
               v-if="$q.screen.gt.xs"
             >
               Joshua Christian Limerson
-            </h1>
-            <h2
+            </h2>
+            <h3
               class="text-white text-weight-bold text-center"
               v-if="$q.screen.xs"
             >
               Joshua Christian Limerson
-            </h2>
+            </h3>
             <div class="row justify-center">
-              <p>
+              <p class="text-h5">
                 I am
-                <span class="typed" :data-typed-items="typedStrings"></span>
+                <span
+                  class="typed text-info"
+                  :data-typed-items="typedStrings"
+                ></span>
               </p>
             </div>
           </q-card-section>
@@ -142,7 +134,7 @@
               <a class="socialContainer containerTwo" href="#">
                 <svg viewBox="0 0 16 16" class="socialSvg twitterSvg">
                   <path
-                    d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
+                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
                   ></path>
                 </svg>
               </a>
@@ -209,7 +201,7 @@ const moveTo = (id: string) => {
   width: 100vw; /* Full width */
   background: linear-gradient(45deg, #d42274, #225ad4, #ff9900);
   background-size: 600% 600%;
-  animation: GradientBackground 10s ease infinite;
+  animation: GradientBackground 30s ease infinite;
 }
 
 @keyframes GradientBackground {
@@ -255,7 +247,7 @@ const moveTo = (id: string) => {
   width: 52px;
   height: 52px;
   border-radius: 50%;
-  background-color: rgb(44, 44, 44);
+  background-color: rgb(50, 50, 50);
   display: flex;
   align-items: center;
   justify-content: center;
